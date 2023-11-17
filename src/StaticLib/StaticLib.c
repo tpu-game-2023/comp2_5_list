73fe5bd074accc962120b6f96cf99015faa70663
imagire <imagire@gmail.com>
2023-11-18 08:57:27 +0900

    add files
    add files
    
    answer

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -23,26 +23,86 @@ void initialize_list(list* l)
 // 先頭にデータを追加
 void push_front(list* l, node* p)
 {
+	if (l == NULL || p == NULL) return;
+
+	p->pNext = l->header;
+	if (l->header) {
+		l->header->pPrev = p;
+	}
+	else {
+		l->footer = p;
+	}
+	l->header = p;
 }
 
 // 末尾にデータを追加
 void push_back(list* l, node* p)
 {
+	if (l == NULL || p == NULL) return;
+
+	p->pPrev = l->footer;
+	if (l->footer) {
+		l->footer->pNext = p;
+	}
+	else {
+		l->header = p;
+	}
+	l->footer = p;
 }
 
 // pのノードを削除
 void remove_node(list* l, node* p)
 {
+	if (l == NULL || p == NULL) return;
+	
+	if (p->pNext) {
+		p->pNext->pPrev = p->pPrev;
+	}
+	else {
+		l->footer = p->pPrev;
+	}
+
+	if (p->pPrev)
+	{
+		p->pPrev->pNext = p->pNext;
+	}
+	else {
+		l->header = p->pNext;
+	}
+
+	p->pNext = p->pPrev = NULL;
 }
 
 // pの次のノードを削除
 void remove_next(list* l, node* p)
 {
+	if (l == NULL || p == NULL || p->pNext == NULL) return;
+
+	node* x = p->pNext;
+	if (x->pNext) {
+		x->pNext->pPrev = p;
+	}
+	else {
+		l->footer = p;
+	}
+	p->pNext = x->pNext;
+	x->pNext = x->pPrev = NULL;
 }
 
 // pの前のノードを削除
 void remove_prev(list* l, node* p)
 {
+	if (l == NULL || p == NULL || p->pPrev == NULL) return;
+
+	node* x = p->pPrev;
+	if (x->pPrev) {
+		x->pPrev->pNext = p;
+	}
+	else {
+		l->header = p;
+	}
+	p->pPrev = x->pPrev;
+	x->pPrev = x->pNext = NULL;
 }
 
 
