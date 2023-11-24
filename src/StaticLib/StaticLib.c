92dd40ae02da718006eafeb63143563d6d287ef4
diceroll5656 <kuni0521iku@icloud.com>
2023-11-24 10:09:33 +0900

    テストを通過しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -1,9 +1,8 @@
 ﻿#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
 #include "Windows.h"                    // Windows API の機能定義
 
+
 #include "../include/lib_func.h"
-
-
 // ノードを初期化
 void initialize_node(node* p, int val)
 {
@@ -11,38 +10,78 @@ void initialize_node(node* p, int val)
 	p->pPrev = NULL;
 	p->data = val;
 }
-
 // リストを初期化
 void initialize_list(list* l)
 {
 	l->header = NULL;
 	l->footer = NULL;
 }
-
-
 // 先頭にデータを追加
 void push_front(list* l, node* p)
 {
+	if (!p)
+		return NULL;
+
+	if (l->footer == NULL)
+		l->footer = p;
+
+	else {
+		p->pNext = l->header;
+		l->header->pPrev = p;
+	}
+
+	p->pPrev = NULL;
+	l->header = p;
 }
 
 // 末尾にデータを追加
 void push_back(list* l, node* p)
 {
+	if (!p)
+		return NULL;
+
+	if (l->header == NULL)
+		l->header = p;
+
+	else {
+		p->pPrev = l->footer;
+		l->footer->pNext = p;
+	}
+
+	p->pNext = NULL;
+	l->footer = p;
 }
 
 // pのノードを削除
 void remove_node(list* l, node* p)
 {
+	if (!p)
+		return NULL;
+
+	if (l->header != p)
+		get_prev(l, p)->pNext = p->pNext;
+	else
+		l->header = p->pNext;
+
+	if (l->footer != p)
+		get_next(l, p)->pPrev = p->pPrev;
+	else
+		l->footer = p->pPrev;
+	initialize_node(p, p->data);
 }
 
 // pの次のノードを削除
 void remove_next(list* l, node* p)
 {
+	p = p->pNext;
+	remove_node(l, p);
 }
 
 // pの前のノードを削除
 void remove_prev(list* l, node* p)
 {
+	p = p->pPrev;
+	remove_node(l, p);
 }
 
 
@@ -51,15 +90,12 @@ node* get_next(list* l, node* p)
 {
 	if (!p) return NULL;
 	return p->pNext;
-
 	return NULL;
 }
-
 // pの前のノードを取得
 node* get_prev(list* l, node* p)
 {
 	if (!p) return NULL;
 	return p->pPrev;
-
 	return NULL;
 }
